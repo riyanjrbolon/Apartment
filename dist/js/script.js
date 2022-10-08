98e2e343a7b709ab5076bc7c4ff78bd23baec929
riyanjrbolon <riyanjrbolon@gmail.com>
2022-10-08 19:47:09 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+const menuBar = document.querySelector(".ri-menu-3-line");
+const menuNav = document.querySelector(".menu");
+const bgNav = document.querySelector(".bg-navbar");
+
+menuBar.addEventListener("click", () => {
+    menuNav.classList.toggle("menu-active");
+    bgNav.classList.toggle("bg-active");
+});
+
